ad77fb77c0e57c5e1dc9d87b262a8d7043d4d304
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-10-28 15:34:35 +0100

    Can POST into notificationRoute
    also added GET

--- api/notificationRoutes.js
+++ api/notificationRoutes.js
@@ -2,12 +2,20 @@ const express = require("express")
 const Notification = require("../schemas/Notification") 
  
 let router = express.Router(); 
- 
+
+
+router.get('/api/notification', async (req, res) => { 
+    let notify = await Notification.find({})
+    res.json(notify)
+}) 
+
 router.post('/api/notification', async (req, res) => { 
     let notify = new Notification(req.body); 
-console.log(notify) 
+    console.log(notify) 
     await notify.save(); 
     res.json(notify) 
  
 }) 
+
+
 module.exports = router
