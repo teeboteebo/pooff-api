0921ca390975709f5ffdd6f7c550b0a43ced371e
Oliver <oliver.nilsson94@gmail.com>
2019-10-28 10:55:04 +0100

    added put route for users

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -3,39 +3,58 @@ const User = require("../schemas/User")
 
 const router = express.Router()
 
-router.get('/api/users', async (req, res) => {
+router.get("/api/users", async (req, res) => {
   let allUsers = await User.find()
   res.status(200).send(allUsers)
 })
-router.post('/api/users', async (req, res) => {
+router.post("/api/users", async (req, res) => {
   let newUser = new User({
-    username:   'DonPooFF',
-    role:       'Don Pooff',
-    password:   '123',
-    firstName:  'Don',
-    lastName:   'Pooff',
-    email:      'pooff@gmail.com',
-    phone:      '070donpooff88',
-    balance:    10000000,
-    darkMode:   true
-    })
+    username: "DonPooFF",
+    role: "Don Pooff",
+    password: "123",
+    firstName: "Don",
+    lastName: "Pooff",
+    email: "pooff@gmail.com",
+    phone: "070donpooff88",
+    balance: 10000000,
+    darkMode: true
+  })
   try {
     await newUser.save()
-    await res.status(200).send({'SAVED': newUser})
-  }
-  catch(err) {
+    await res.status(200).send({ SAVED: newUser })
+  } catch (err) {
     res.send(err)
   }
 })
 
-router.delete('/api/users/delete', async (req, res) => {
+router.put("/api/users/id/:id/edit", async (req, res) => {
+  let user = await User.findById(req.params.id)
+  user.username = req.body.content.username
+  user.role = req.body.content.role
+  user.password = req.body.content.password
+  user.firstName = req.body.content.fistName
+  user.lastName = req.body.content.lastName
+  user.email = req.body.content.email
+  user.phone = req.body.content.phone
+  user.balance = req.body.content.balance
+  user.darkMode = req.body.content.darkMode
+
+  recipe.save(function(err) {
+    if (err) {
+      next(err)
+    } else {
+      res.status(200).send()
+    }
+  })
+})
+
+router.delete("/api/users/delete", async (req, res) => {
   try {
     let userToDelete = await User.findById(req.body.id)
     userToDelete.delete()
-    res.status(200).send('User found and deleted!')
-  }
-  catch(err) {
-    res.send('No such user found')
+    res.status(200).send("User found and deleted!")
+  } catch (err) {
+    res.send("No such user found")
   }
 })
 
