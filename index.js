d4c501e4799804f1b01a74d57dcca570608ff099
Oliver <oliver.nilsson94@gmail.com>
2019-10-29 13:36:33 +0100

    activate/inactivate route in user

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const mailRoutes = require("./API/mailRoutes")
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const settings = require('./config/settings.json')
-const connectToDb = require('./config/db')
-const userRoutes = require('./api/userRoutes')
-const qnaRoutes = require('./api/qnaRoutes')
-const counterRoute = require('./api/counterRoute')
-const loginRoutes = require('./api/loginRoutes')
-const aclRules = require('./config/acl-rules.json')
-const acl = require('./middleware/acl')
+const express = require("express")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
+const settings = require("./config/settings.json")
+const connectToDb = require("./config/db")
+const userRoutes = require("./api/userRoutes")
+const qnaRoutes = require("./api/qnaRoutes")
+const counterRoute = require("./api/counterRoute")
+const loginRoutes = require("./api/loginRoutes")
+const mailRoutes = require("./api/mailRoutes")
+const aclRules = require("./config/acl-rules.json")
+const acl = require("./middleware/acl")
 
 connectToDb()
 
@@ -34,23 +34,28 @@ app.use(
 
 app.use(acl(aclRules))
 
-app.use(userRoutes, loginRoutes, qnaRoutes, counterRoute)
+app.use(userRoutes, loginRoutes, qnaRoutes, counterRoute, mailRoutes)
 
 // let all user roles have access get mytransactions
 // because the "acl" in this case take place inside the route
 // by only looking up transactions belonging to the logged in user
-app.get('/api/mytransactions',  async (req, res) => {
-  let user = req.session.user;
-  if(!user){ res.json([]); return; }
+app.get("/api/mytransactions", async (req, res) => {
+  let user = req.session.user
+  if (!user) {
+    res.json([])
+    return
+  }
   // lookup transactions involving me in the db
   // given that amount in a transactions is always a positive number
   // then reverse the number to negative for everyhing i sent
-  let iGot = await Transaction.find({receiver: user._id});
-  let iSent = await Transaction.find({sender: user._id}).map(x => ({...x, amount: -x.amount}));
-  let allMyTransactions = iGot.concat(iSent);
-  allMyTransactions.sort((a,b) => a.date < b.date ? -1 : 1);
-  res.json(allMyTransactions);
+  let iGot = await Transaction.find({ receiver: user._id })
+  let iSent = await Transaction.find({ sender: user._id }).map(x => ({
+    ...x,
+    amount: -x.amount
+  }))
+  let allMyTransactions = iGot.concat(iSent)
+  allMyTransactions.sort((a, b) => (a.date < b.date ? -1 : 1))
+  res.json(allMyTransactions)
 })
-
 
 app.listen(5000, () => console.log(`Pooff Server is on port 5000`))
