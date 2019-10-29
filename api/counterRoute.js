4dfdb780b2f0512fb4690e45c3a357f1100a76a1
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-29 10:30:56 +0100

    Route for updating qna counter

---
+++ api/counterRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Qna = require('./schemas/Qna')
+
+const router = express.Router()
+
+router.put('/api/counter/:id', async(req, res) => {
+  const qna = await Qna.findById(req.params.id)
+  qna.counter++
+  qna.save(err => {
+    if (err) return
+    res.status(200).json('Counter updated!')
+  })
+})
+
+module.exports = router
