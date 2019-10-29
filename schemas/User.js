1b45a59eb5809e4cf4273d079527ca997fc3ff65
Jesper Asplund <jesper.asplund95@gmail.com>
2019-10-29 15:14:19 +0100

    added children to model and changed back to transactions history

--- schemas/User.js
+++ schemas/User.js
@@ -3,14 +3,20 @@ const Schema = mongoose.Schema
 
 let userSchema = new Schema({
   username:     { type: String, unique: true },
-  role:         { type: String, default: 'pooffer' },
+  role:         { type: String, default: 'user' },
   password:     { type: String },
   firstName:    { type: String },
   lastName:     { type: String },
   email:        { type: String, unique: true },
   phone:        { type: String, unique: true },
   active:       { type: Boolean, default: false },
-  darkMode:     { type: Boolean, default: false }
+  darkMode:     { type: Boolean, default: false },
+  transactions: [
+    {type: Schema.Types.ObjectId, ref: 'Transaction'}
+  ],
+  children:     [
+    {type: Schema.Types.ObjectId, ref: 'User'}
+  ]
 })
 
 module.exports = mongoose.model("User", userSchema)
