277007970f1cdf03d006fb22a62d23361a44ed6f
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-10-29 10:39:54 +0100

    Notifcation delete fixed
    we can delete notification atleast

--- api/notificationRoutes.js
+++ api/notificationRoutes.js
@@ -3,19 +3,36 @@ const Notification = require("../schemas/Notification")
  
 let router = express.Router(); 
 
-
+//find all notification that has been posted
 router.get('/api/notification', async (req, res) => { 
     let notify = await Notification.find({})
-    res.json(notify)
+    res.status(200).send(notify)
 }) 
 
+
+//create notification
 router.post('/api/notification', async (req, res) => { 
-    let notify = new Notification(req.body); 
-    console.log(notify) 
-    await notify.save(); 
-    res.json(notify) 
- 
+    try{
+        let notify = new Notification(req.body.content); 
+        await notify.save(); 
+        res.json(notify)
+    }
+    catch (err) {
+        res.send("Could not post notification")
+      }
 }) 
+
+
+//delete a notification
+router.delete('/api/notification/delete', async (req, res) =>{
+ try{
+     let notifyToDelete = await Notification.findById(req.body.id)
+     notifyToDelete.delete()
+     res.status(200).send("Notification found and deleted!")
+    } catch (err) {
+        res.send("No such notification found")
+      }
+ })
 
 
 module.exports = router
