7d5a4c52755a743bf6fdc62e1fe4832b540461d5
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-30 12:01:11 +0100

    Route for parent to get children transactions

--- api/myUserRoutes.js
+++ api/myUserRoutes.js
@@ -9,9 +9,9 @@ router.get('/api/myuser', async (req, res) => {
 })
 router.put('/api/myuser', async (req, res) => {
   let user = await User.findById(req.session.user._id)
-  let updatedUser = {...req.body}
+  let updatedUser = { ...req.body }
   // if role somehow is tried to manipulate, delete the role from body
-  if(updatedUser.role) delete updatedUser.role
+  if (updatedUser.role) delete updatedUser.role
   Object.assign(user, updatedUser)
   await user.save()
   res.json(user)
@@ -24,6 +24,17 @@ router.put('/api/godaddy', async (req, res) => {
   res.json(user)
 })
 
+router.get('/api/mychildren', async (req, res) => {
+  const user = await User.findById(req.session.user._id)
+
+  const myChildrenTransactions = []
+  for (let child of user.children){
+    const me = await User.findById(child).populate('transactions').select('transactions firstName lastName').exec()
+    myChildrenTransactions.push(me)
+  }
+
+  res.json(myChildrenTransactions)
+})
 
 module.exports = router
 
