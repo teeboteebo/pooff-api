7cfd5eac9ccecacbc87855a750953a0fb65ba303
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-30 13:47:43 +0100

    Added salt to passwords

--- config/encryptPassword.js
+++ config/encryptPassword.js
@@ -2,6 +2,6 @@ const crypto = require('crypto')
 const settings = require('../config/settings.json')
 
 module.exports = password => {
-  return crypto.createHmac('sha256', salt)
+  return crypto.createHmac('sha256', settings.salt)
     .update(password).digest('hex');
 }
