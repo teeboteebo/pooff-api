7e9e95baf420ebada820176ac11661e8c978fc1b
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-12 14:32:19 +0100

    db back to cloud

--- config/db.js
+++ config/db.js
@@ -3,15 +3,15 @@ const connectionString = require('./connectionString.js')
 
 const connectToDb = () => {
   // connect to db for testing
-  mongoose.connect('mongodb://localhost/testPooff', {
+  mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
   })
-  // mongoose
-  //   .connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  //   .then(() => console.log("MongoDB connected"))
-  //   .catch(err => console.log(err))
+  mongoose
+    .connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
 
   global.db = mongoose.connection
 }
